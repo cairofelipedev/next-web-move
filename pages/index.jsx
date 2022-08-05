317d038cc598f5eda8c6ab48ff23a1339f886b16
Cairo <cairofelipedev@gmail.com>
2022-08-04 23:42:59 -0300

    Colunistas and youtube channel

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,7 +4,13 @@ import Container from "@components/container";
 import { useRouter } from "next/router";
 import { getClient, usePreviewSubscription } from "@lib/sanity";
 import defaultOG from "../public/img/opengraph.jpg";
-import { postquery, configQuery, destaques, destaques2 } from "@lib/groq";
+import {
+  postquery,
+  configQuery,
+  destaques,
+  destaques2,
+  authorsquery
+} from "@lib/groq";
 import GetImage from "@utils/getImage";
 import PostList from "@components/postlist";
 import DestaqueItem from "@components/destaqueitem";
@@ -17,9 +23,18 @@ import { Navigation, FreeMode, Scrollbar, Mousewheel } from "swiper";
 import "@css/style.module.css";
 import PostList2 from "@components/postlist2";
 import PostList3 from "@components/postlist3";
+import YoutubeEmbed from "@components/Youtube";
+import Image from "next/image";
 
 export default function Post(props) {
-  const { postdata, siteconfig, preview, postdata2, postdata3 } = props;
+  const {
+    postdata,
+    siteconfig,
+    preview,
+    postdata2,
+    postdata3,
+    authors
+  } = props;
 
   const router = useRouter();
 
@@ -77,8 +92,8 @@ export default function Post(props) {
             <div className="relative flex flex-col justify-center overflow-hidden bg-white py-6 sm:py-4">
               <div className="mx-auto w-full rounded bg-[#BFBFBF] p-20 text-center"></div>
             </div>
-            <div className="flex">
-              <div className="w-2/3">
+            <div className="md:flex">
+              <div className="md:w-2/3">
                 <Swiper
                   navigation={true}
                   modules={[Navigation]}
@@ -94,7 +109,7 @@ export default function Post(props) {
                   ))}
                 </Swiper>
               </div>
-              <div className="w-1/3">
+              <div className="md:w-1/3">
                 <Swiper
                   slidesPerView={2}
                   spaceBetween={5}
@@ -117,12 +132,7 @@ export default function Post(props) {
                 </Swiper>
               </div>
             </div>
-            {/* {posts2.map(item => (
-              <div key={item.id}>
-                <DestaqueItem destaques={item} />
-              </div>
-            ))} */}
-            <div className="grid gap-10 lg:gap-10 md:grid-cols-2 ">
+            <div className="grid gap-10 lg:gap-10 md:grid-cols-2 pt-8">
               {posts.slice(0, 2).map(post => (
                 <PostList
                   key={post._id}
@@ -132,6 +142,10 @@ export default function Post(props) {
                 />
               ))}
             </div>
+            <p className="text-center pt-4">Publicidade</p>
+            <div className="relative flex flex-col justify-center overflow-hidden bg-white py-6 sm:py-4">
+              <div className="mx-auto w-full rounded bg-[#BFBFBF] p-10 text-center"></div>
+            </div>
             <div className="grid gap-10 mt-10 lg:gap-10 md:grid-cols-2 xl:grid-cols-3 ">
               {posts.slice(2).map(post => (
                 <PostList
@@ -140,6 +154,34 @@ export default function Post(props) {
                   aspect="square"
                 />
               ))}
+            </div>
+            <div className="md:flex pt-6 pb-6">
+              <div className="md:w-2/3">
+                <YoutubeEmbed embedId="cOV3bz0HQmc" />
+              </div>
+              <div className="md:w-1/3 pt-8 md:pt-2">
+                <h1 className="text-center text-2xl font-bold">Colunistas</h1>
+                {authors.slice(0, 3).map(author => {
+                  const { width, height, ...imgprops } = GetImage(
+                    author?.image
+                  );
+                  return (
+                    <div className="flex items-center gap-3 px-4 py-4" key={author._id}>
+                      <div className="relative flex-shrink-0 w-24 h-24">
+                        <Image
+                          {...imgprops}
+                          objectFit="cover"
+                          layout="fill"
+                          alt={author.name || " "}
+                          placeholder="blur"
+                          className="rounded-full"
+                        />
+                      </div>
+                      <span className="text-xl text-gray-900">{author.name}</span>
+                    </div>
+                  );
+                })}
+              </div>
             </div>
             <p className="text-center">Publicidade</p>
             <div className="relative flex flex-col justify-center overflow-hidden bg-white py-6 sm:py-4">
@@ -157,11 +199,13 @@ export async function getStaticProps({ params, preview = false }) {
   const post2 = await getClient(preview).fetch(destaques);
   const post3 = await getClient(preview).fetch(destaques2);
   const config = await getClient(preview).fetch(configQuery);
+  const authors = await getClient(preview).fetch(authorsquery);
 
   // const categories = (await client.fetch(catquery)) || null;
 
   return {
     props: {
+      authors: authors,
       postdata: post,
       postdata2: post2,
       postdata3: post3,
